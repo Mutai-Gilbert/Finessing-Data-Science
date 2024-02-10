fe5557e56e8edd04eac9080547d3282b247e5019
mutai-gilbert <mutaigilbert85@gmail.com>
2024-02-10 22:42:56 +0300

    Add new changes

---
+++ pythonfromscratch.py
@@ -0,0 +1,25 @@
+# defaultdict
+
+word_count = {}
+for word in document:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+word_counts = {}
+
+for word in document:
+    try:
+        word_counts[word] += 1
+    except  KeyError:
+        word_counts[word] = 1
+
+
+
+from collections import defaultdict
+
+word_count = defaultdict(int)
+for word in document:
+    word_counts[word] += 1
+    
