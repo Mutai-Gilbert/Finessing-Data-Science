4fcfae69e43a7423625dd93360aa6b1187c45d9c
mutai-gilbert <mutaigilbert85@gmail.com>
2024-02-12 21:15:54 +0300

    Add new changes

--- pythonfromscratch.py
+++ pythonfromscratch.py
@@ -29,3 +29,9 @@ from collections import Counter
 c = Counter([0, 1, 2, 0])
 
 print(c)
+
+stopwords_list = ["a", "an", "at"] + hundreds_of_other_words + ["yet", "you"]
+"zip" in stopwords_list
+
+stopwords_set = set(stopwords_list)
+"zip" in stopwords_set
