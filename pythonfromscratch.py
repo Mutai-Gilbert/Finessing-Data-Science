e4a6bff88f780dcd6cbab5327693dec18b2a092f
mutai-gilbert <mutaigilbert85@gmail.com>
2024-02-13 22:24:01 +0300

    Add new changes

--- pythonfromscratch.py
+++ pythonfromscratch.py
@@ -35,3 +35,13 @@ stopwords_list = ["a", "an", "at"] + hundreds_of_other_words + ["yet", "you"]
 
 stopwords_set = set(stopwords_list)
 "zip" in stopwords_set
+
+from matplotlib import pyplot as pyplot
+
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+
+plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
+plt.title("Nominal GDP")
+plt.ylabel("Billions of $")
+plt.show()
